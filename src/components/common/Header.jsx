a2c7f4ab4c4efddac57e8e6a8ad364861f7c75c1
Mikayla M <54966992+verdetea22@users.noreply.github.com>
2022-04-02 20:01:00 -0400

    Create Header.jsx

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import "./NewListing.css";
+
+
+const NewListing = () => {
+    return (
+        <div className="background">
+
+            <h1><b>New Listing</b></h1>
+
+            <div>
+
+            </div>
+
+
+
+        </div>
+    )
+}
+
+
+
+
+export default NewListing;
