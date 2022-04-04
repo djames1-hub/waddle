acaf46a698468e843d7836e82a650740625958ad
Mikayla M <54966992+verdetea22@users.noreply.github.com>
2022-04-04 15:50:16 -0400

    Add files via upload

---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <div className="background">
+            <h1><b>Home</b></h1>
+            <p>
+            another implementation of search bar,<br/>
+            rectangle previews of items w clickable icons, <br/>
+            list of categories to sort by on side
+            </p>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/pages/index.js
@@ -0,0 +1,5 @@
+import Dropdown from './newListing/dropdown';
+
+export {
+   Dropdown,
+}
