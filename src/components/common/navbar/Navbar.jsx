5ccf8db7a2a0b9d2b4cb8fa7bd49bdbed4dbc469
Matt Mandelbaum <mattrmandelbaum@gmail.com>
2022-04-05 22:03:11 -0400

    navbar stays to right side

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -7,15 +7,19 @@ import './Navbar.css';
 function Navbar () {
 
     return (
-    <section className="navbar">
-         <a href="/" className="navbar-item">Home</a>
-                      <a href="/newListing" className="navbar-item">New Listing</a>
-                      <a href="/cart" className="navbar-item">Cart</a>
-                      <a href="/wishlist" className="navbar-item">Wishlist</a>
-                      <a href="/settings" className="navbar-item">Settings</a>
-                      <a href="/sign-up" className="navbar-item">Account</a>
-         < Searchbar placeholder="Search for an item..." data={ItemData} />
-    </section>
+    <div className="navbar">
+        <div className="header-left">
+            <a href="/" className="navbar-item">Home</a>
+        </div>
+            <div className="header-right">
+                    <a href="/newListing" className="navbar-item">New Listing</a>
+                    <a href="/cart" className="navbar-item">Cart</a>
+                    <a href="/wishlist" className="navbar-item">Wishlist</a>
+                    <a href="/settings" className="navbar-item">Settings</a>
+                    <a href="/sign-up" className="navbar-item">Account</a>
+                    < Searchbar placeholder="Search for an item..." data={ItemData} />
+            </div>      
+    </div>
     )
 }
 
