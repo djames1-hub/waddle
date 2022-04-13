99e4e62ffb289a1a74b7d6db8f42c8d8111d49e5
Matt Mandelbaum <mattrmandelbaum@gmail.com>
2022-04-13 00:13:56 -0400

    styling header

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -4,6 +4,9 @@ import './Navbar.css';
 
 const Navbar = () => {
     
+    // create hooks to monitor input changes
+    const [search, setSearch] = useState("");
+
     return (
     <section className="navbar">
          <a href="/" className="navbar-item">Home</a>
@@ -12,6 +15,10 @@ const Navbar = () => {
                       <a href="/wishlist" className="navbar-item">Wishlist</a>
                       <a href="/settings" className="navbar-item">Settings</a>
                       <a href="/sign-up" className="navbar-item">Account</a>
+                      <div id="searchbar-container">
+                          <input type="text" className="search-input" placeholder="Search" onChange={event => setSearch(event.target.value)}/>
+                          <input type="button" className="search-button" value="Search" onClick={() => {window.location.href="/search/"+search}}/>
+                      </div>
     </section>
     )
 }
