1d60bba65a0c5191ccedcec0b1e8b36298209762
verdetea22 <mikayla.mount22@gmail.com>
2022-04-26 13:14:11 -0400

    adding to settings, got signout to work

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,30 +1,51 @@
-import React from 'react';
+import React from "react";
 
-import { auth } from '../../services/firebase/firebase-config';
-import { onAuthStateChanged } from 'firebase/auth';
-import { signOutUser } from './../../services/firebase/users';
-import "./Settings.css"
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import SettingsButton from './settingsButton';
+import { auth } from "../../services/firebase/firebase-config";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { signOutUser } from "./../../services/firebase/users";
+import "./Settings.css";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import SettingsButton from "./settingsButton";
 
 //import images
-import backArrow from "./icons/backarrow.png"
-
+import backArrow from "./icons/backarrow.png";
 
 const Settings = () => {
+  onAuthStateChanged(auth, async (user) => {
+    if (user) {
+    } else {
+      window.location.href = "/login";
+    }
+  });
 
-    onAuthStateChanged(auth, async (user) => {
-        if(user) {
-        }else{
-            window.location.href = "/login";
-        } 
-    });
+  return (
+    <Container>
+      <br />
+      <Row>
+        <Button
+          onClick={() => {
+            window.location.href = "/purchase-history";
+          }}
+        >
+          Purchase History
+        </Button>
+      </Row>
+      <br />
+      <Row>
+        <Button
+          onClick={() => {
+            window.location.href = "/user";
+          }}
+        >
+          View User Details
+        </Button>
+      </Row>
+      <br />
+      <Row>
+        <button onClick={signOutUser}>Sign Out</button>
+      </Row>
+    </Container>
+  );
+};
 
-    return (<Container>
-                <Row>
-                    <Button onClick={() => {window.location.href ="/purchase-history"}}>Purchase History</Button>
-                </Row>
-            </Container>)
-}
-
-export default Settings
+export default Settings;
