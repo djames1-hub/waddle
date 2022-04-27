4c25ef75c6aff9ef9e62353a8e896f56bcc3504d
verdetea22 <mikayla.mount22@gmail.com>
2022-04-26 21:16:51 -0400

    removed settings and account from header, needs single listing route

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { Cart, NotFound, Header } from './components';
-import { Home, Wishlist, ListingForm, SignUp, SignIn, Search, ItemView, BulkListing } from './pages';
-import './App.css';
-import { FirebaseAuthProvider } from './contexts';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Cart, NotFound, Header } from "./components";
+import {
+  Home,
+  Wishlist,
+  ListingForm,
+  SignUp,
+  SignIn,
+  Search,
+  ItemView,
+  BulkListing,
+} from "./pages";
+import "./App.css";
+import { FirebaseAuthProvider } from "./contexts";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    return (
-        <div className="App">
-            <FirebaseAuthProvider>
-                <Header />
-                {/* ROUTING */}
-                <div>
-                <Router>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/wishlist" element={<Wishlist />} />
-                        <Route path="/cart" element={<Cart />} />
-                        <Route path="/new-listing" element={<ListingForm />} />
-                        <Route path="/bulk-listing/:category" element={<BulkListing />} />
-                        <Route path="/not-found" element={ <NotFound />} />
-                        <Route path="/sign-up" element={ <SignUp />} />
-                        <Route path="/login" element={ <SignIn />} />
-                        <Route path="/view-item/:id" element={ <ItemView />} />
-                        <Route path="/search/*" element={ <Search />} />
-                        <Route path="/*" element={ <NotFound />} />
-                    </Routes>
-                </Router>
-                </div>
-            </FirebaseAuthProvider>
+  return (
+    <div className="App">
+      <FirebaseAuthProvider>
+        <Header />
+        {/* ROUTING */}
+        <div>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/wishlist" element={<Wishlist />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/new-listing" element={<ListingForm />} />
+              <Route path="/bulk-listing/:category" element={<BulkListing />} />
+              <Route path="/not-found" element={<NotFound />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/login" element={<SignIn />} />
+              <Route path="/view-item/:id" element={<ItemView />} />
+              <Route path="/search/*" element={<Search />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </Router>
         </div>
+      </FirebaseAuthProvider>
+    </div>
   );
 }
 
