e3410126ced65e54db5a7d1e79d9dec3efb2b475
verdetea22 <mikayla.mount22@gmail.com>
2022-04-26 23:11:23 -0400

    cri

--- src/pages/ListingForm/ListingForm.jsx
+++ src/pages/ListingForm/ListingForm.jsx
@@ -1,25 +1,73 @@
+<<<<<<< HEAD
 import React, {  useState } from 'react';
 import {  Button, Form, ButtonGroup, ButtonToolbar }  from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import { useFirebaseAuth } from '../../hooks/useFirebaseAuth';
 import { LoadingScreen } from '../../components';
+=======
+import React, { useEffect, useState } from "react";
+import {
+  Card,
+  Nav,
+  Button,
+  Form,
+  ButtonGroup,
+  Stack,
+  ButtonToolbar,
+} from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { Timestamp } from "firebase/firestore";
+import { v4 as uuidv4 } from "uuid";
+
+import { createListing } from "./../../services/firebase/listings";
+
+import { useFirebaseAuth } from "../../hooks/useFirebaseAuth";
+import { LoadingScreen } from "../../components";
+>>>>>>> small fix
 
 const ListingForm = () => {
-    
-    const { id, address } = useFirebaseAuth();
-    const [form, setForm] = useState(<LoadingScreen />);
-    const [listingType, setListingType] = useState("");
-    const [category, setCategory] = useState("");
-    const [listingInfo, setListingInfo] = useState({});
-    const [variations, setVariations] = useState([]);
-    const { register, handleSubmit } = useForm();
-    const [formIterator, setFormIterator] = useState(0);
+  const { id, address } = useFirebaseAuth();
+  const [form, setForm] = useState(<LoadingScreen />);
+  const [listingType, setListingType] = useState("");
+  const [category, setCategory] = useState("");
+  const [listingInfo, setListingInfo] = useState({});
+  const [variations, setVariations] = useState([]);
+  const { register, handleSubmit } = useForm();
+  const [formIterator, setFormIterator] = useState(0);
 
+<<<<<<< HEAD
     const submit = ({ listingType, category }) => {
         window.location.href = `/${listingType}/${category}`;
     }
+=======
+  const submitListing = ({ listingData, itemData }) => {
+    let listing = {
+      listingId: uuidv4(),
+      seller: id,
+      buyer: "",
+      dateBought: new Timestamp.fromDate(new Date()),
+      quantity: 1,
+      isPurchased: false,
+      shippingCost: 0.0,
+      item: {
+        itemId: uuidv4(),
+        itemName: itemData.itemName,
+        props: itemData.props,
+      },
+      shippingFrom: { ...address },
+      category,
+      ...listingData,
+    };
 
+    createListing(listing);
+  };
+>>>>>>> small fix
 
+  const submit = ({ listingType, category }) => {
+    window.location.href = `/${listingType}/${category}`;
+  };
+
+<<<<<<< HEAD
     return (
         <>
             <Form className='w-50 mx-auto border p-3' onSubmit={handleSubmit(submit)}>     
@@ -55,5 +103,41 @@ const ListingForm = () => {
     );
     
 }
+=======
+  return (
+    <>
+      <br />
+      <br />
+      <Form className="w-50 mx-auto border" onSubmit={handleSubmit(submit)}>
+        <Form.Group className="mb-3 mx-5 mt-3" controlId="listingTypeControl">
+          <Form.Label>Choose Listing Type</Form.Label>
+          <Form.Select
+            {...register("listingType", {
+              required: "Must not be empty",
+            })}
+          >
+            <option value="">Listing Types...</option>
+            <option value="single-listing">Single Listing</option>
+            <option value="bulk-listing">Bulk Listing</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3 mx-5 mt-3" controlId="categories">
+          <Form.Label>Categories</Form.Label>
+          <Form.Select {...register("category")}>
+            <option>Choose category</option>
+            <option value="books">Book</option>
+            <option value="clothing">Clothing</option>
+            <option value="furniture">Furniture</option>
+            <option value="electronics">Electronics</option>
+            <option value="sports-gear">Sports Gear</option>
+          </Form.Select>
+        </Form.Group>
+        <Button type="submit">Continue</Button>
+        <br />
+      </Form>
+    </>
+  );
+};
+>>>>>>> small fix
 
 export default ListingForm;
