61e74da188ebce02ef2ea858ec384213e7699e96
Dylan James <djjames455@gmail.com>
2022-05-02 07:49:18 -0400

    Refactor sendNotification and change notifications

--- src/services/firebase/users/notifications.js
+++ src/services/firebase/users/notifications.js
@@ -1,66 +1,26 @@
-import { updateDoc, doc, setDoc, Timestamp, getDoc } from "firebase/firestore";
-import { db } from "./../firebase-config";
+import { createTransport } from 'nodemailer';
 
-class Notification{
-    constructor(message, time, uid, isRead){
-        this.message = message;
-        this.time = time;
-        this.uid = uid;
-        this.isRead = isRead;
-    }
-}
-
-const sendNotification = async (notifications, uid, message) => {
-    console.log(uid, message);
-    const userRef = doc(db, "users", uid);
-    try {
-        let notification = {
-            message,
-            time: Timestamp.fromDate(new Date()),
-            uid,
-            isRead: false
+export const sendNotification = ({ email, credentials, notificationMessage }) => {
+    const waddleEmail = '';
+    let transporter = createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
         }
-        await updateDoc(userRef, { notifications: [notification, ...notifications]});
-    } catch (error) {
-        return error
-    }
-};
+    });
 
-const getNotifications = async (uid) => {
-    try {
-        const userRef = doc(db, "users", uid);
-        const docSnap = await getDoc(userRef);
-        return docSnap.data().notifications;
-    } catch (error) {
-        return error;
+    const message = {
+        from: waddleEmail,
+        to: email,
+        subject: notificationMessage.subject,
+        text: notificationMessage.message,
+        html: notificationMessage.html
     }
-}
 
-const notificationConverter = {
-    toFirestore: (notification) => {
-        return {
-            message: notification.message,
-            time: notification.time,
-            isRead: notification.isRead
-        };
-    },
-    fromFirestore: (snapshot, options) => {
-        const data = snapshot.data(options);
-        return new Notification(data.message, data.time, data.uid, data.isRead);
-    }
-}
-
-const markAllRead = async (notifications, uid) => {
-    let newNotifs = [];
-    for(let notif of notifications){
-        const tempNotif = notif;
-        tempNotif.isRead = true;
-        newNotifs = [...newNotifs, tempNotif];
-    }
-    const ref = doc(db, "users", uid).withConverter(notificationConverter);
-    await setDoc( {
-        notifications: newNotifs
+    transporter.sendMail(message, (err, info) => {
+        if (err) {
+            console.log(err);
+        }
     });
 }
-
-export { markAllRead, sendNotification, getNotifications ,Notification };
