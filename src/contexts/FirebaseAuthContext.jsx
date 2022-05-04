eba31ac2905ce9674c8865a3e2bea56722706d4e
Dylan James <djjames455@gmail.com>
2022-05-04 19:21:50 -0400

    Stop FirebaseAuthContext to stop infinte updating

--- src/contexts/FirebaseAuthContext.jsx
+++ src/contexts/FirebaseAuthContext.jsx
@@ -7,26 +7,20 @@ import { getCurrentUser } from "../services/firebase/users";
 const FirebaseAuthContext = createContext();
 
 const FirebaseAuthProvider = ({ children }) => {
-    const [id, setId] = useState(null);
-    const [user, setUser] = useState({});
+    const [value, setValue] = useState({});
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, async (u) => {
-            setId((u) ? u.uid: null);
+            if (u.uid) {
+                const user = await getCurrentUser(u.uid);
+                setValue({ id: u.uid, phoneNumber: u.phoneNumber, ...user });
+            }
         });
         return unsubscribe;
-    }, [id]);
-
-    useEffect(() => {
-        const getUser = async () => {
-            let user = await getCurrentUser(id);
-            setUser({ id, ...user });
-        }
-        return getUser;
-    }, [user]);
+    }, []);
 
     return (
-        <FirebaseAuthContext.Provider value={user}>
+        <FirebaseAuthContext.Provider value={value}>
             {children}
         </FirebaseAuthContext.Provider>
     );
